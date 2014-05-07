3df1dbdb2e2fd69ddc2b139a3ce9ff192b9542a8
Dakota Z. Derryberry <dakotaz@utexas.edu>
2014-05-07 13:58:28 -0500

    m -r .git/COMMIT_EDITMSG was saying, working on overlap file, but not convinced that it works, because finding no overlap which I don't believe...

--- ANALYSIS/overlap.py
+++ ANALYSIS/overlap.py
@@ -19,7 +19,7 @@ def check_file_existence( filter_level, double_samples ):
 
     for sample in double_samples:
         existence = []
-        print double_samples[ sample ]
+        # print double_samples
         for dir in double_samples[ sample ]:
             file = os.path.join( dir, filter_level )
             if os.path.exists( file ):
@@ -28,7 +28,7 @@ def check_file_existence( filter_level, double_samples ):
             num_doub += 1
             useable_double_samples[ sample ] = double_samples[ sample ]
 
-    print "there are" + num_doub + "useable samples"
+    print "there are %g useable samples..." % num_doub
 
     return useable_double_samples
 
@@ -56,13 +56,50 @@ def data_locs( file ):
     return dict
 
 ## this function
-def compare_locs( filt_dict, unfilt_dict ):
+## the hypothesis states that a substantial portion of the mutations in dictA (time_0 and WGS) should also be in dictB (time_1 and WGA)
+def compare_locs( dictA, dictB ):
 
+    all = {}
     overlap = {}
 
-    
+    # set up chrom keys in overlap and all
+    chroms = []
+    for chrom in dictB:
+        if chrom not in chroms:
+            chroms.append( chrom )
+    for chrom in dictA:
+        if chrom not in chroms:
+            chroms.append( chrom )
+    for chrom in chroms:
+        all[ chrom ] = []
+        if chrom in dictA and chrom in dictB:
+            overlap[ chrom ] = []
 
-    return overlap
+    # now do the compare from the point of view of dictB
+    for chrom in dictB:
+        for loc in dictB:
+            if chrom in dictA:
+                if loc in dictA[ chrom ]:
+                    overlap[ chrom ].append( loc )
+                    all[ chrom ].append( loc )
+                else:
+                    all[ chrom ].append( loc )
+            else:
+                all[ chrom ].append( loc )
+
+    # and from the point of view of dictA, given all overlap with dictB has already been found
+    for chrom in dictA:
+        for loc in dictA[ chrom ]:
+            if loc not in all[ chrom ]:
+                all[ chrom ].append( loc )
+
+    # sort all and overlap for my peace of mind
+    for chrom in all:
+        all[ chrom ] = sorted( all[ chrom ] )
+    for chrom in overlap:
+        overlap[ chrom ] = sorted( overlap[ chrom ] )
+
+    return all, overlap
 
 ###################
 ## MAIN FUNCTION ##
@@ -71,14 +108,28 @@ def compare_locs( filt_dict, unfilt_dict ):
 ## pretty sure C282 is WGA and C484 WGS. Will double check that with Matt and update this note, however. 
 
 # first thing is to load the doubles files
-double_samples = VCF_list.analyzed_doubles()
+double_samples, doub_count = VCF_list.analyzed_doubles()
 
 # and check to make sure that all the files you need are there
 filter_level = "unfiltered.txt"
 useable_double_samples = check_file_existence( filter_level, double_samples )
 
 # now run the comparison
+# currently assuming that C282 is WGA and C484 is WGS
+for sample in useable_double_samples:
+    C282_dict = {}
+    C484_dict = {}
+    for dir in useable_double_samples[ sample ]:
+        file = os.path.join( dir, filter_level )
+        if "C282" in file:
+            C282_dict = data_locs( file )
+            # print "made WGA daict..."
+        elif "C484" in file:
+            C484_dict = data_locs( file )
+            # print "made WGS dict..."
+    # print C484_dict
+    all, overlap = compare_locs( C484_dict, C282_dict )
+    print overlap
 
-
-
-# and save the data in /ANALYSIS/FIGURES, so that you can generate figures...
+# and save the data in /GBM_genomics/FIGURES/FIGURE_DATA, so that you can generate figures...
+print 

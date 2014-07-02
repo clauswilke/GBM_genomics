9af093b5b668da22899d17da6d600b04a328a141
Dakota Z. Derryberry <dakotaz@utexas.edu>
2014-07-02 12:08:09 -0500

    added filter_by_filter for the overlap files

---
+++ ANALYSIS/overlap_only_filter_by_filter.py
@@ -0,0 +1,3 @@
+## this script does the same thing as filter_by_filter.py,
+## which is to see which filters are removing which mutations,
+## but it does so for only the putative mutations in the 55 double samples that are in the unfiltered overlap

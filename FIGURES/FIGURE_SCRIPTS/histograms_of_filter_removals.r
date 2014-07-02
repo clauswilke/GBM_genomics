b216b3b8edbd4c51fcbcac4c93972cfa7f92f394
Dakota Z. Derryberry <dakotaz@utexas.edu>
2014-07-02 11:58:51 -0500

    not sure what is going on, apparently have competing changes on two computers

--- FIGURES/FIGURE_SCRIPTS/histograms_of_filter_removals.r
+++ FIGURES/FIGURE_SCRIPTS/histograms_of_filter_removals.r
@@ -2,4 +2,4 @@
 
 # first look at the STD_40 data
 # first, just everything removed, each file individually, how many by each filter 3-8
-data = read.table( "../FIGURE_DATA/histograms_of_filter_removals.r", header=T )
+data = read.table( "../FIGURE_DATA/unfiltered_WGS-WGA_counts.txt", header=T )

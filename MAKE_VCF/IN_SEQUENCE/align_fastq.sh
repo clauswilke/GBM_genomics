67dbdeb2f56c95bf34cd6e3665cc3192eeb45da0
Dakota Derryberry <dakotaz@login1.stampede.tacc.utexas.edu>
2014-08-23 12:00:00 -0500

    added job script for aligning fastq

--- MAKE_VCF/IN_SEQUENCE/align_fastq.sh
+++ MAKE_VCF/IN_SEQUENCE/align_fastq.sh
@@ -2,31 +2,38 @@
 
 # the purpose of this script is to align the two fastq files of a TCGA sample (paired ends) to hg19
 
-module load bwa
-module load samtools
+ml bwa
+ml samtools
 
 # pfx is the sample name
 pfx=$1
+queue="normal"
+# reference hg19
 refDir="/work/00001/mattcowp/Hs_reference_datasets"
 hgReference="$refDir/Homo_sapiens.GRCh37.72.dna.fa"
-queue="normal"
+# what data we're running on...
+data_dir="/scratch/00001/mattcowp/dakota/"
+fastq1="$data_dir/$pfx/$pfx.1.fastq"
+fastq2="$data_dir/$pfx/$pfx.2.fastq"
 
 # move to working directory
-cd /scratch/00001/mattcowp/dakota/$pfx
+cd $data_dir/$pfx
+
+# the bwa manual says this is important
+bwa index -p RefSeqbwaidx -a bwtsw $hgReference
 
 # bwa aln first fastq
-bwa aln -q 30 -t 4 $refindex $f3fastqfile > $f3fastqfile_prefix.sai 2>$f3fastqfile_prefix.bwa.log
+bwa aln -q 30 -t 4 $hgReference $fastq1 > $fastq1_prefix.sai 2>$fastq1_prefix.bwa.log
 # bwa aln second fastq
-bwa aln -q 30 -t 4 $refindex $f5fastqfile > $f5fastqfile_prefix.sai 2>$f5fastqfile_prefix.bwa.log
+bwa aln -q 30 -t 4 $hgReference $fastq2 > $fastq2_prefix.sai 2>$fastq2_prefix.bwa.log
 echo "bwa aln for $f5fastqfile done"
 
+# need to specify read groups (RG), which is the argument of  -r in bwa sempe
+RG="@RG\tID:1\tPL:ILLUMINA\tSM:$pfx\tLB:$LB\tDS:ref=hg19,pfx="$refindex
+
 # bwa sampe | samtools view
-(bwa sampe -a 600 -P -r "$RG" $refindex $f3fastqfile_prefix.sai $f5fastqfile_prefix.sai $f3fastqfile $f5fastqfile | samtools view -bSh -o $outprefix.bam -) 2>$outprefix.sampe.log
+(bwa sampe -a 600 -P -r "$RG" $hgReference $fastq1_prefix.sai $fastq2_prefix.sai $fastq1 $fastq2 | samtools view -bSh -o $outprefix.bam -) 2>$outprefix.sampe.log
 # samtools sort
 samtools sort $outprefix.bam $outprefix.sorted >>$outprefix.sampe.log
 # samtools index
 samtools index $outprefix.sorted.bam >>$outprefix.sampe.log
-
-
-
-* rename bams so aligned to hg19

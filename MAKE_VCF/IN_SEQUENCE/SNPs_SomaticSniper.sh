a6d0373581f69e147ea6119928d82171b0e8197b
Dakota Derryberry <dakotaz@login4.stampede.tacc.utexas.edu>
2014-09-06 15:13:50 -0500

    more of the in sequence VCF making

--- MAKE_VCF/IN_SEQUENCE/SNPs_SomaticSniper.sh
+++ MAKE_VCF/IN_SEQUENCE/SNPs_SomaticSniper.sh
@@ -1,1 +1,8 @@
-# this takes a single sample's normal and tumor hg19-aligned bams, and returns a VCF, through the software SomaticSniper
+#!/bin/bash
+
+## this takes a single sample's normal and tumor hg19-aligned bams, and returns a VCF, through the software SomaticSniper
+
+# load modules
+
+
+## variant calling

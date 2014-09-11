a59ebd5b53601bcd815ef55fbe4c7b9993d5c9bd
Dakota Derryberry <dakotaz@login2.stampede.tacc.utexas.edu>
2014-09-11 11:20:38 -0500

    debugging teh GATK processing still...

--- JOB_SCRIPTS/dedup_realn_recal.sh
+++ JOB_SCRIPTS/dedup_realn_recal.sh
@@ -1,1 +1,19 @@
-# job script
+#! /bin/bash
+
+#SBATCH -J t1_3.1
+#SBATCH -o t1_3.1.out
+#SBATCH -e t1_3.1.err
+#SBATCH -n 16
+#SBATCH -p normal
+#SBATCH -A mattcowp
+#SBATCH -t 20:00:00
+
+set -x                      # Echo commands
+
+echo `hostname`
+
+# Run the executables
+cd /scratch/00001/mattcowp/dakota
+bash /home1/01839/dakotaz/GBM_genomics/MAKE_VCF/IN_SEQUENCE/SNPs_GATK.sh C484.TCGA-06-0125-02A-11D-2280-08.1 C484.TCGA-06-0125-10A-01D-1490-08.6
+
+bash /home1/01839/dakotaz/GBM_genomics/MAKE_VCF/IN_SEQUENCE/SNPs_SomaticSniper.sh C484.TCGA-06-0125-02A-11D-2280-08.1 C484.TCGA-06-0125-10A-01D-1490-08.6
--- MAKE_VCF/IN_SEQUENCE/SNPs_SomaticSniper.sh
+++ MAKE_VCF/IN_SEQUENCE/SNPs_SomaticSniper.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-## load modules
-
 ## command line variables (this is taken from the previous script, GATK_SNPs.sh)
 tumor_pfx=$1
 blood_pfx=$2
@@ -16,22 +14,16 @@ if [ $tumor_sample != $blood_sample ]; then
 	exit 1;
 fi
 
+tumor_dir="$tumor_pfx\_working"
+cd /scratch/00001/mattcowp/dakota/$tumor_dir
+
 ## files you may need...
-$output_dir=""
 ref_dir="/work/00001/mattcowp/Hs_reference_datasets"
 hgReference="$ref_dir/Homo_sapiens.GRCh37.72.dna.fa"
 
 ## run SomaticSniper on a set of T-N pairs
-echo "SomaticSniper: " 'date' >> ../$tumor_sample.log
+echo "SomaticSniper: " 'date' >> ../$tumor_pfx.ss.log
 /scratch/00001/mattcowp/dakota/bam-somaticsniper -q 40 -Q 40 -J -s 0.001 -F vcf -f $hgReference $tumor_pfx.realn.recal.bam $blood_pfx.realn.recal.bam $tumor_pfx.SS.vcf
+echo "SomaticSniper finished: " 'date' >> ../$tumor_pfx.ss.log
 
-## make sure the file exists, was made, then exit
-if [ -f $tumor_pfx.SS.vcf ]; then
-	cd ..
-	mv tumor_sample/$tumor_pfx.SS.vcf $output_dir
-	mv tumor_sample.log $output_dir/$tumor_pfx.log
-	echo "SomaticSniper run: " 'date' >> $output_dir/$tumor_pfx.log
-	exit 0;
-fi;
-
-exit1;
+exit;

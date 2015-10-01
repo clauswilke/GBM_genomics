d9d29c2d029bf0971c3ff1daa390f82f1d90b698
DakotaZ <dakota@Dakotas-MacBook-Air.local>
2015-10-01 16:39:52 -0500

    fixed figure numbering mistake

--- FIGURES/FIGURE_SCRIPTS/Figures.R
+++ FIGURES/FIGURE_SCRIPTS/Figures.R
@@ -114,11 +114,11 @@ names( LOH_VAQ_data )[ names( LOH_VAQ_data )=="FILT_7_PERCENT" ] <- "LOH_PERCENT
 # note that this is pretty close to 1.00 every time, hence the inversion...
 LOH_VAQ_data$SUM=LOH_VAQ_data$LOH_PERCENT + LOH_VAQ_data$VAQ_PERCENT
 
-#pdf( "../FIGURE_PDFS/Figure6.pdf" )
+pdf( "../FIGURE_PDFS/Figure6.pdf" )
 par(bty='n', mfrow=c(1,2) )
 plot( LOH_VAQ_data$OVERLAP, LOH_VAQ_data$LOH_PERCENT, xlab="Number of SNVs in both replicates", ylab="Percentage of SNVs in both replicates filtered out by LOH", pch=20 )
 plot( LOH_VAQ_data$OVERLAP, LOH_VAQ_data$VAQ_PERCENT, xlab="Number of SNVs in both replicates", ylab="Percentage of SNVs in both replicates filtered out by VAQ", pch=20 )
-#dev.off()
+dev.off()
 
 
 ##############
